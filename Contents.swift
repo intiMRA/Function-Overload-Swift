33e04218f45a8c9853f4aaf5a2850bfa71a3717a
inti <inti.albuquerque@trademe.co.nz>
2024-01-19 08:45:23 +1300

    update code

--- Contents.swift
+++ Contents.swift
@@ -2,44 +2,26 @@
 
 import Foundation
 
-protocol Animal {
-    func eat(food: String, kgs: Int)
-    func eat(food: String)
-}
-
-extension Animal {
-    func eat(food: String = "corn") {
-        self.eat(food: food)
-    }
-}
-
-class Dog: Animal {
+class Dog {
     let name: String
     
     init(name: String) {
         self.name = name
     }
-    func eat(food: String = "dog food", kgs: Double = 1.1) {
-        print("\(name) eats precisely \(kgs) kgs of \(food) everyday")
-    }
-    
-    func eat(food: String = "dog food", kgs: Int = 1) {
-        print("\(name) eats \(kgs) kgs of \(food) everyday")
-    }
-    
-    func eat(food: String = "dog food") {
-        print("\(name) eats \(food) everyday")
+    @_disfavoredOverload
+    func eat() {
+        print("A Dog named \(name) eats food everyday")
     }
 }
 
 class GoldenRetriever: Dog {
-    override func eat(food: String = "dog food", kgs: Double = 1.1) {
-        print("A golden retriever named \(name) eats precisely \(kgs) kgs of \(food) everyday")
+    func eat(food: String = "dog food") {
+        print("A GoldenRetriever named \(name) eats \(food) everyday")
     }
 }
 
 let dog = Dog(name: "Rex")
-dog.eat(food: "meat", kgs: 1.8)
+dog.eat()
 let retriever = GoldenRetriever(name: "Max")
-retriever.eat(food: "meat", kgs: 4.0)
+retriever.eat()
 
